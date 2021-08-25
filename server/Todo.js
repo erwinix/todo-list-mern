972b5badda4d7ec6756ea5f896930264a5fa8833
DeveloperJS <erwin.18.2012@gmail.com>
2021-08-24 23:06:48 -0400

    TODO-LIST PROJECT SUCCESSFULLY

---
+++ server/Todo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let todoSchema = new Schema({
+    task: {
+        type: String
+    }
+}, {
+    collection: 'todos'
+});
+
+module.exports = mongoose.model('Todo', todoSchema)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import AddTodoComponent from './components/addTodo.component';
+import ViewTodoComponent from './components/viewTodo.component';
+
+function App() {
+    return (
+      <div className="App container mt-5">
+        <AddTodoComponent />
+        <ViewTodoComponent />
+      </div>
+    );
+}
+
+export default App;
